6eb22a7226d32975e40200ca6400530668343831
Nate Treimer <natetreimer@gmail.com>
2021-04-12 14:28:23 -0500

    addMovie objectToSend

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -1,27 +1,37 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {Link} from "react-router-dom";
 
 function MovieList() {
-
     const dispatch = useDispatch();
-    // const movies = useSelector(store => store.movies);
+    const [title, setTitle] = useState('');
+    const [imageURL, setImageURL] = useState('');
+    const [description, setDescription] = useState('');
+    const [genre, setGenre] = useState('');
 
-    // useEffect(() => {
-    //     dispatch({ type: 'FETCH_MOVIES'});
-    // }, []);
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const objectToSend = {
+            title: title,
+            imageURL: imageURL,
+            description: description,
+            genre: genre
+        }
+        console.log(objectToSend);
+        dispatch({type: 'ADD_MOVIE', payload: objectToSend})
+    }
 
     return (
         <main>
             <h1>Add Movie</h1>
-            <form action="" id="usrform">
-                <input type="text" placeholder="Movie Title"/>
-                <input type="text" placeholder="URL for poster"/>
+            <form id="usrform">
+                <input type="text" placeholder="Movie Title" value={title} onChange={(event) => setTitle(event.target.value)}/>
+                <input type="text" placeholder="URL for poster" value={imageURL} onChange={(event) => setImageURL(event.target.value)}/>
                 <br/>
-                <textarea rows="4" cols="50" form="usrform">Enter movie description...</textarea>
+                <textarea rows="4" cols="50" form="usrform" value={description} onChange={(event) => setDescription(event.target.value)}></textarea>
                 <br/>
                 <label htmlFor="genres">Choose a genre:</label>
-                <select name="genres" id="">
+                <select name="genres" id="" value={genre} onChange={(event) => setGenre(event.target.value)}>
                     <option value="Adventure">Adventure</option>
                     <option value="Animated">Animated</option>
                     <option value="Biographical">Biographical</option>
@@ -36,9 +46,11 @@ function MovieList() {
                     <option value="Space-Opera">Space-Opera</option>
                     <option value="Superhero">Superhero</option>
                 </select>
+                <button>Cancel</button>
+                <button onClick={handleSubmit}>Save</button>
+                {/* <input type="submit" value="Save"/> */}
             </form>
-            <button>Cancel</button>
-            <button>Save</button>
+            
         </main>
 
     );
