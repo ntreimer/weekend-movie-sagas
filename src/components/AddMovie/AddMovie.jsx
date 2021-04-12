a8f2cedd52b4ba6f57946194564408fd766d1936
Nate Treimer <natetreimer@gmail.com>
2021-04-12 10:33:27 -0500

    added AddMovie form

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -15,10 +15,30 @@ function MovieList() {
         <main>
             <h1>Add Movie</h1>
             <form action="" id="usrform">
-                <input type="text"/>
-                <input type="text"/>
-                <textarea rows="4" cols="50" form="usrform">Enter description...</textarea>
+                <input type="text" placeholder="Movie Title"/>
+                <input type="text" placeholder="URL for poster"/>
+                <br/>
+                <textarea rows="4" cols="50" form="usrform">Enter movie description...</textarea>
+                <br/>
+                <label htmlFor="genres">Choose a genre:</label>
+                <select name="genres" id="">
+                    <option value="Adventure">Adventure</option>
+                    <option value="Animated">Animated</option>
+                    <option value="Biographical">Biographical</option>
+                    <option value="Comedy">Comedy</option>
+                    <option value="Disaster">Disaster</option>
+                    <option value="Drama">Drama</option>
+                    <option value="Epic">Epic</option>
+                    <option value="Fantasy">Fantasy</option>
+                    <option value="Musical">Musical</option>
+                    <option value="Romantic">Romantic</option>
+                    <option value="Science Fiction">Science Fiction</option>
+                    <option value="Space-Opera">Space-Opera</option>
+                    <option value="Superhero">Superhero</option>
+                </select>
             </form>
+            <button>Cancel</button>
+            <button>Save</button>
         </main>
 
     );
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,7 +2,7 @@ import {HashRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList';
 import MovieDetails from '../MovieDetails/MovieDetails';
-
+import AddMovie from '../AddMovie/AddMovie'
 function App() {
   return (
     <div className="App">
